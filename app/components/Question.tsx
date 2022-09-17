0a8df6cf75ae211acf8e201ef747e5af85c299fb
Jan Monschke <janmonschke@fastmail.com>
2022-09-17 13:20:20 +0300

    styling

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -17,7 +17,7 @@ export default function QuestionComponent({
   return (
     <div className="flex flex-col gap-2">
       <div>
-        <span className="text-xl">{questionText}</span>
+        <span className="text-lg">{questionText}</span>
       </div>
       <div>Points: {points}</div>
       {answerOptions && (
